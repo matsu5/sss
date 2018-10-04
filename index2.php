15a6146a9102772d9b732581752f982acf5e2e39
matsu <matsu55sho@hotmail.co.jp>
2018-10-04 16:19:15 +0900

    first commit

---
+++ index2.php
@@ -0,0 +1,17 @@
+#cat /var/log/php.error_log
+Array
+(
+  [webhook_setting_id] => 736
+  [webhook_event_type] => message_created
+  [webhook_event_time] => 1519019750
+  [webhook_event] => Array
+    (
+      [message_id] => 1017313389526647584
+      [room_id] => 1234567
+      [account_id] => 1234321
+      [body] => おはよう！
+      [send_time] => 1519019750
+      [update_time] => 0
+    )
+)
+#
